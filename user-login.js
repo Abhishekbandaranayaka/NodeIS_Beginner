92ce3c3986c805213fa3a040d6ebda0f96e16291
Abhishek Rathnayaka <89740367+Abhishekbandaranayaka@users.noreply.github.com>
2023-06-25 15:47:51 +0530

    c9

---
+++ user-login.js
@@ -0,0 +1,14 @@
+//input
+const userName = "Abhishek";
+const userPass = "123"
+
+
+//DB
+const dBUserName = "Abhishek";
+const dBPass = "123";
+
+if((userName===dBUserName) && (userPass===dBPass)){
+    console.log("Welcome");
+}else{
+    console.log("Try again");
+}
